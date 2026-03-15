28ec3149695c959c5203bc12e5955d1e14baad02
Rajsab <rajasabsayyed123@gmail.com>
2026-03-15 10:57:43 +0530

    clean project structure

--- campus-project-website/app/api/sos/test-db/route.ts
+++
@@ -1,7 +0,0 @@
-import { connectDB } from "@/lib/db"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  await connectDB()
-  return NextResponse.json({ message: "DB Connected ✅" })
-}
